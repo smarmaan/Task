2d956ed391d6e484fb6a00db0dbf48953defe18f
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-03 23:51:29 +0600

    withdraw file updated...

--- Baper Bank 2.0/withdraw.js
+++ Baper Bank 2.0/withdraw.js
@@ -6,6 +6,17 @@ document.getElementById("withdraw-btn").addEventListener("click", function () {
   const previousWithdrawTotal = getTextElementValueById(
     "base-withdrawn-amount"
   );
+  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
 
+  const previousTotalBalance = getTextElementValueById("base-balance-amount");
 
+  const newBalanceTotal = previousTotalBalance - newWithdrawAmount;
+
+  if (previousTotalBalance < newWithdrawAmount) {
+    alert("Oi bal.. Baper bank e eto tk nai");
+    return 0;
+  } else {
+    setInnerTextByID("base-withdrawn-amount", currentWithdrawTotal);
+    setInnerTextByID("base-balance-amount", newBalanceTotal);
+  }
 });
